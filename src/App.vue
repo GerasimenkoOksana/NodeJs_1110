c80a58a1865d3eb2e09bc80e2e78dbbe80e414f4
Oksana Gerasimenko <gerrus155@gmail.com>
2021-10-11 17:46:46 +0300

    1110

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NovaPoshta/>
+    <HelloWorld msg="hello"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NovaPoshta from '@/components/NovaPoshta'
+
 
 export default {
   name: 'App',
   components: {
+    NovaPoshta,
     HelloWorld
   }
 }
